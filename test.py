9e95cd6d35da77615f04a43847dd923a6b06b31f
Frederick Zhang <fredzhang2013@gmail.com>
2021-07-22 21:26:43 -0700

    improvements in format, and added another model

--- test.py
+++ test.py
@@ -27,32 +27,15 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
-# for the smallerRetinaVein:
-classifiers = [
-    KNeighborsClassifier(3), # 93.6
-    # SVC(kernel="linear", C=0.025), # 50.7
-    # GaussianProcessClassifier(1.0 * RBF(1.0)), #91.6 takes much longer than others
-    DecisionTreeClassifier(max_depth=5), #90.6
-    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1), # 85.9
-    MLPClassifier(alpha=1, max_iter=1000), #64.8
-    AdaBoostClassifier(), #89.8
-    GaussianNB(), #82.03
-    QuadraticDiscriminantAnalysis()] #89.1; variables are colinear warning
-    # for Quadratic Discrim:
-# precision: [0.87231869 0.91255869]
-# recall: [0.91976306 0.86149584]
-# fscore: [0.89541284 0.88629239]
-# support: [1857 1805]
-
 
 start_time = time.time()
 
-# data = cPickle.loads(open("models/smallerRetinaVeinData.cpickle", "rb").read())
-# labels = cPickle.loads(open("models/smallerRetinaVeinLabels.cpickle", "rb").read())
+data = cPickle.loads(open("models/smallerRetinaVeinData.cpickle", "rb").read())
+labels = cPickle.loads(open("models/smallerRetinaVeinLabels.cpickle", "rb").read())
 # model = cPickle.loads(open("models/smallerRetinaVein.cpickle", "rb").read())
 
-data = cPickle.loads(open("models/retinaVeinData.cpickle", "rb").read())
-labels = cPickle.loads(open("models/retinaVeinLabels.cpickle", "rb").read())
+# data = cPickle.loads(open("models/retinaVeinData.cpickle", "rb").read())
+# labels = cPickle.loads(open("models/retinaVeinLabels.cpickle", "rb").read())
 # model = cPickle.loads(open("models/retinaVein.cpickle", "rb").read())
 
 
@@ -65,22 +48,49 @@ labels = cPickle.loads(open("models/retinaVeinLabels.cpickle", "rb").read())
 # labels = cPickle.loads(open("models/smallerRetinaVeinLabels.cpickle", "rb").read())
 # model = cPickle.loads(open("models/retinaVein.cpickle", "rb").read())
 
-for c in classifiers:
-    print("A")
-    model = c.fit(data, labels)
-    predictions = model.predict(data)
-    score = accuracy_score(labels, predictions)
-    print(score)
 
-    print((f1_score(labels, predictions, average="macro")))
-    print((precision_score(labels, predictions, average="macro")))
-    print((recall_score(labels, predictions, average="macro")))
 
-    precision, recall, fscore, support = precision_recall_fscore_support(labels, predictions)
 
-    print('precision: {}'.format(precision))
-    print('recall: {}'.format(recall))
-    print('fscore: {}'.format(fscore))
-    print('support: {}'.format(support))
+# for the smallerRetinaVein:
+classifiers = [
+    ("knn", KNeighborsClassifier(3)), # 93.6
+    ("SVC", SVC(kernel="linear", C=0.025)), # 50.7
+    # ("GPC", GaussianProcessClassifier(1.0 * RBF(1.0))), #91.6 takes much longer than others
+    ("Decision Tree", DecisionTreeClassifier(max_depth=5)), #90.6
+    ("Random Forest", RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)), # 85.9
+    ("MLP, alpha1", MLPClassifier(alpha=1, max_iter=1000)), #64.8
+    ("AdaBoost", AdaBoostClassifier()), #89.8
+    ("GaussianNB", GaussianNB()), #82.03
+    ("MLP hidden layer", MLPClassifier(hidden_layer_sizes=(3), max_iter=10000000)), # 86.2
+    ("Quadratic Discrim", QuadraticDiscriminantAnalysis())#89.1; variables are colinear warning
+]
 
-print(time.time() - start_time)
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.15)
+
+file = open("results/model-results-smallerRetinaVein.txt", "w")
+file.write("Results of different AI models on a mini-set of data for retina veins. "
+           "Total 3665 images: 15% for testing. \n")
+
+for t in classifiers:
+    c = t[1]
+    file.write(t[0] + "\n")
+    model = c.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    score = accuracy_score(y_test, predictions)
+    file.write(str(score) + "\n")
+
+    file.write(str((f1_score(y_test, predictions, average="macro"))) + "\n")
+    file.write(str((precision_score(y_test, predictions, average="macro"))) + "\n")
+    file.write(str((recall_score(y_test, predictions, average="macro"))) + "\n")
+
+    precision, recall, fscore, support = precision_recall_fscore_support(y_test, predictions)
+
+    file.write('precision: {}'.format(precision) + "\n")
+    file.write('recall: {}'.format(recall) + "\n")
+    file.write('fscore: {}'.format(fscore) + "\n")
+    file.write('support: {}'.format(support) + "\n")
+    file.write("--------------------" + "\n")
+
+file.write("Time Taken: " + str(time.time() - start_time) + "\n")
+
+file.close()
